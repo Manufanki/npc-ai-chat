9a3428d504002297c51f5787c2ed9f4419d85230
Manufanki <Manufanki@gmail.com>
2024-06-26 20:49:38 +0200

    inital commit

---
+++ npc-ai-chat.js
@@ -0,0 +1,24 @@
+
+
+//Global variables
+export const moduleName = "ai-npc-chat";
+export let configDialog;
+
+
+
+Hooks.on('ready',()=>{
+    console.log("ai-npc-chat ready"); 
+
+});
+
+
+
+/**
+ * Init hook
+ * Initialize settings
+ */
+Hooks.once('init', function(){
+    console.log("ai-npc-chat INIT");
+
+})
+
